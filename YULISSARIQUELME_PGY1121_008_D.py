f283236515af8538469e21813ba18193676a3d55
Fernanda <136729254+feninvuwu@users.noreply.github.com>
2023-07-11 12:08:55 -0400

    Add files via upload

---
+++ YULISSARIQUELME_PGY1121_008_D.py
@@ -0,0 +1,94 @@
+import  
+
+
+def mostrar_MENU():
+
+    print("""MENÚ:
+    1.Comprar entradas
+    2.Mostrar ubicaciones disponibles
+    3.Ver listados de asistentes
+    4.Mostrar ganancias totales
+    5.Salir""")
+
+def opcion_MENU():
+    try
+    opcion = int(input("Ingrese la opción que sea adquirir"))
+    if opcion in(1,2,3,4,5);
+
+listas_filas []
+listas_columnas []
+
+
+else:
+
+    print("Debe seleccionar una de las opciones")
+
+except:
+
+    print("ERROR! DEBE DE SELECCIONAR ALGUNA OPCIÓN MOSTRADA ANTERIORMENTE")
+
+
+def mostrar_Escenario():
+
+    print ("  ______________________  ") 
+    print ( "|                      | ")
+    print ( "|                      | ")
+    print ( "|                      | ")
+    print ( "|                      | ")
+    print ( "|      ESCENARIO       | ")
+    print ( "|                      | ")
+    print ( "|                      | ") 
+    print ( "|                      | ")
+    print ( "|______________________| ")
+
+
+for x input (10:)
+print("columna (1,,2,3,4,4,5,6,7,8,9,10)")
+
+
+def precio():
+    print("""Precios
+    patium  $120.000   (asientos del 1 al 20)
+    Gold    $80.000    (asientos del 21 al 50) 
+    Silver  $50.000    (asientos del 51 al 100)""")
+
+
+acumu_cant_platinum = 0
+acumu_cant_gold = 0
+acumu_cant_silver = 0
+
+os.sistem
+cant_entrada = cant_entrada ()
+
+
+
+
+
+
+
+
+Tipo de entrada     | Cantidad |     Total
+
+Platinum  $120.000       2         $240.000
+Gold      $80.000        4         $320.000
+Silver    $50.000        10        $50.000
+TOTAL                    16        $1.060.000
+
+
+
+
+
+
+
+
+
+
+
+
+
+def (salir):
+    print("GRACIAS POR SU COMPRA,DISFRUTE SU FUNCIÓN")
+    break
+
+
+
